37687a9fb41367060ebf37f577614c0eb1788b36
Pecon <admin@pecon.us>
2016-09-28 13:47:49 -0700

    Removed google custom search
    I'll do something for searching again later.

--- index.php
+++ index.php
@@ -242,19 +242,7 @@
 				break;
 				
 			case "search":
-				print("<script>
-  (function() {
-    var cx = '005671575927562648658:5-28rwkueqw';
-    var gcse = document.createElement('script');
-    gcse.type = 'text/javascript';
-    gcse.async = true;
-    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-        '//cse.google.com/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(gcse, s);
-  })();
-</script>
-<gcse:search></gcse:search>");
+				print("");
 				break;
 				
 			default:
