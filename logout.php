9bad4e2081f8af20786d2975aea987be100308b9
Pecon <admin@pecon.us>
2016-10-26 09:25:42 -0700

    Revert "Revert "Added email verification and password reset functionality""
    This reverts commit 0c3cee0da05259c447f843d36d7e9cd26ac7e2bd.

--- logout.php
+++ logout.php
@@ -2,8 +2,7 @@
 	session_start();
 	$pageTitle = "REforum - Logout";
 	$metaTags = "<meta HTTP-EQUIV=\"Pragma\" content=\"no-cache\">
-				<meta HTTP-EQUIV=\"Expires\" content=\"-1\">
-				<meta http-equiv=\"refresh\" content=\"3; url=./\">";
+				<meta HTTP-EQUIV=\"Expires\" content=\"-1\">";
 	require_once './header.php';
 	print("<center>");
 	require_once './navmenu.php';
@@ -12,7 +11,7 @@
 	if(isSet($_SESSION['loggedin']))
 	{
 		session_destroy();
-		print("You are now logged out.<br><a href=\"./\">Back</a>");
+		print("You are now logged out.<br><a href=\"./\">Back</a><script> window.setTimeout(function(){window.location.href = \"./\";}, 1500);</script>");
 	}
 	else
 	{
