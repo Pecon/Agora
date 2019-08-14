3bb3dc080e128b6c29c551546651867038355c52
Pecon <admin@pecon.us>
2019-08-14 05:07:23 -0700

    Fix some visual problems when viewing post edits.

--- themes/twilight/edits.php
+++ themes/twilight/edits.php
@@ -32,7 +32,7 @@
 	print("<div class=\"topicHeader\"><span>&nbsp;&rarr;&nbsp;</span><h3>Post edits</h3></div>");
 
 
-	print('<div class="post originalPost postBackgroundA">');
+	print('<div class="post topPost postBackgroundA">');
 	print("\n<div class=\"postUser\"><a class=\"userLink\" name=\"${user['id']}\"></a><a class=\"userLink\" href=\"./?action=viewProfile&amp;user=${user['id']}\">${username}</a>");
 
 	// Display the user's tagline
@@ -45,7 +45,7 @@
 
 	print("<img class=\"avatar\" src=\"./avatar.php?user=${user['id']}&amp;cb=${user['avatarUpdated']}\" /><div class=\"postDate finetext\">${date}<br />(Current version)</div><div class=\"userPostSeperator\"></div></div>");
 
-	print("\n<div class=\"postBody\"><div class=\"postText\">${post['postPreparsed']}</div></div></div>");
+	print("\n<div class=\"postBody\"><div class=\"postText\">${post['postPreparsed']}</div><div class=\"postFooter\"></div></div></div>");
 
 
 	
@@ -73,7 +73,7 @@
 		$changeID = $change['lastChange'];
 		$date = date("F d, Y H:i:s", $change['changeTime']);
 
-		print('<div class="post originalPost ' . ($iterations % 2 ? "postBackgroundB" : "postBackgroundA") . '">');
+		print('<div class="post ' . ($iterations % 2 ? "postBackgroundB" : "postBackgroundA") . '">');
 		print("\n<div class=\"postUser\"><a class=\"userLink\" name=\"${user['id']}\"></a><a class=\"userLink\" href=\"./?action=viewProfile&amp;user=${user['id']}\">${username}</a>");
 
 		// Display the user's tagline
@@ -86,13 +86,13 @@
 
 		print("<img class=\"avatar\" src=\"./avatar.php?user=${user['id']}&amp;cb=${user['avatarUpdated']}\" /><div class=\"postDate finetext\">${date}</div><div class=\"userPostSeperator\"></div></div>");
 
-		print("\n<div class=\"postBody\"><div class=\"postText\">${changeText}</div></div></div>");
+		print("\n<div class=\"postBody\"><div class=\"postText\">${changeText}</div><div class=\"postFooter\"></div></div></div>");
 
 		$iterations++;
 	}
 
 	$date = date("F d, Y H:i:s", $post['postDate']);
-	print('<div class="post originalPost ' . ($iterations % 2 ? "postBackgroundB" : "postBackgroundA") . '">');
+	print('<div class="post ' . ($iterations % 2 ? "postBackgroundB" : "postBackgroundA") . '">');
 	print("\n<div class=\"postUser\"><a class=\"userLink\" name=\"${user['id']}\"></a><a class=\"userLink\" href=\"./?action=viewProfile&amp;user=${user['id']}\">${username}</a>");
 
 	// Display the user's tagline
@@ -105,6 +105,6 @@
 
 	print("<img class=\"avatar\" src=\"./avatar.php?user=${user['id']}&amp;cb=${user['avatarUpdated']}\" /><div class=\"postDate finetext\">${date}<br />(Original)</div><div class=\"userPostSeperator\"></div></div>");
 
-	print("\n<div class=\"postBody\"><div class=\"postText\">${change['postData']}</div></div></div>");
+	print("\n<div class=\"postBody\"><div class=\"postText\">${change['postData']}</div><div class=\"postFooter\"></div></div></div>");
 	print('<div class="topicFooter"></div></div>');
 ?>
