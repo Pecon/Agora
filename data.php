3e4b8a41e9b88efcc38422c62ef588dea010a8c8
Pecon <admin@pecon.us>
2021-05-05 11:12:35 -0700

    Revert unfinished changes to data.php that were breaking setup
    This reverts commit 16f9f759ab936fcf9a79e18f106bec7550973ec9.

--- data.php
+++ data.php
@@ -8,7 +8,7 @@
 	$settings = json_decode(file_get_contents("./data/.settings.json"), true);
 
 	if($settings === null)
-		exit("JSON parser error: " . json_last_error_msg() . " <br \>As a result, unable to read config file. Please report this as a bug with your settings.json file attached (Remember to censor your database password).");
+		exit("JSON parser error: " . json_last_error_msg() . " <br>As a result, unable to read config file. Please report this as a bug with your settings.json file attached (Remember to censor your database password).");
 
 	if(isSet($settings['sql_server_address']))
 		$servername = $settings['sql_server_address'];
@@ -42,89 +42,67 @@
 		error("Missing server config: sql_database_name");
 	}
 
-
-	// Agora global config
-
-	$sql = "SELECT * FROM `globalSettings`;";
-	$result = querySQL($sql);
-
-	if($result === false)
-	{
-		error("Unable to query global settings. Something is very wrong.");
-		return;
-	}
-
-	$result = $result -> fetch_all();
-
-	global $globalSettings;
-	$globalSettings = Array();
-
-	foreach($result as $setting)
-	{
-		$globalSettings[$setting['settingName']] = $setting['settingValue'];
-	}
-
-
-	if(isSet($globalSettings['site_timezone']))
-		$site_timezone = $globalSettings['site_timezone'];
+	if(isSet($settings['site_name']))
+		$site_name = $settings['site_name'];
 	else
 	{
 		require_once 'page.php';
-		error("Missing global setting: site_timezone");
+		error("Missing server config: site_name");
+	}
+	
+	if(isSet($settings['site_timezone']))
+		$site_timezone = $settings['site_timezone'];
+	else
+	{
+		require_once 'page.php';
+		error("Missing server config: site_timezone");
 
 		date_default_timezone_set($site_timezone);
 	}
 
-	if(isSet($globalSettings['site_name']))
-		$site_name = $globalSettings['site_name'];
+	if(isSet($settings['require_email_verification']))
+		$require_email_verification = $settings['require_email_verification'];
 	else
 	{
 		require_once 'page.php';
-		error("Missing global setting: site_name");
+		error("Missing server config: require_email_verification");
 	}
 
-	if(isSet($globalSettings['require_email_verification']))
-		$require_email_verification = $globalSettings['require_email_verification'];
+	if(isSet($settings['min_password_length']))
+		$min_password_length = $settings['min_password_length'];
 	else
 	{
 		require_once 'page.php';
-		error("Missing global setting: require_email_verification");
+		error("Missing server config: min_password_length");
 	}
 
-	if(isSet($globalSettings['min_password_length']))
-		$min_password_length = $globalSettings['min_password_length'];
+	if(isSet($settings['force_ssl']))
+		$force_ssl = $settings['force_ssl'];
 	else
 	{
 		require_once 'page.php';
-		error("Missing global setting: min_password_length");
+		error("Missing server config: force_ssl");
 	}
 
-	if(isSet($globalSettings['force_ssl']))
-		$force_ssl = $globalSettings['force_ssl'];
+	if(isSet($settings['items_per_page']))
+		$items_per_page = $settings['items_per_page'];
 	else
 	{
 		require_once 'page.php';
-		error("Missing global setting: force_ssl");
+		error("Missing server config: items_per_page");
 	}
 
-	if(isSet($globalSettings['items_per_page']))
-		$items_per_page = $globalSettings['items_per_page'];
+	if(isSet($settings['theme']))
+		$site_theme = $settings['theme'];
 	else
 	{
 		require_once 'page.php';
-		error("Missing global setting: items_per_page");
+		error("Missing server config: theme");
 	}
 
-	if(isSet($globalSettings['theme']))
-		$site_theme = $globalSettings['theme'];
-	else
-	{
-		require_once 'page.php';
-		error("Missing global setting: theme");
-	}
 
-	if(isSet($globalSettings['show_eastereggs']))
-		$show_eastereggs = $globalSettings['show_eastereggs'];
+	if(isSet($settings['show_eastereggs']))
+		$show_eastereggs = $settings['show_eastereggs'];
 	else
 		$show_eastereggs = false;
 ?>
